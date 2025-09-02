666e3edbafd53d350dfe3f74061d8b0a2e18480a
Jessica Luhahe <jessicaluhahe@MacBook-Air-de-Jessica.local>
2025-09-02 10:16:40 +0200

    install vite bundle analyzer for analyse bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// plungin for analyse bundle
+import { analyzer } from 'vite-bundle-analyzer'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    analyzer()
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
